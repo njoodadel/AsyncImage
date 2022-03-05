5f33b3e761ebc7d9a032ee3b504c32ef67154359
Njood <njoodalshaalan.66@gmail.com>
2022-03-05 13:55:32 +0300

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Njood on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
